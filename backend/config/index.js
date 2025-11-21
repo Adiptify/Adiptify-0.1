194aa337dddc6368df27f333eb08adbacd58c963
Kumar Aditya <130602883+Adiptify@users.noreply.github.com>
2025-11-22 01:25:11 +0530

    Update MongoDB URI in config file

--- backend/config/index.js
+++ backend/config/index.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 export const config = {
   port: process.env.PORT || 4000,
-  mongoUri: process.env.MONGO_URI || "mongodb+srv://kumaradityaprof_db_user:Sarthak%402004@adiptify.cqpfa9f.mongodb.net/nimbus?retryWrites=true&w=majority&appName=Adiptify",
+  mongoUri: process.env.MONGO_URI || "mongodb+srv://yourowndata:Sthak%40cqpfa9f.mome=Adiptify",
   mongoDb: process.env.MONGO_DB || "nimbus",
   jwtSecret: process.env.JWT_SECRET || "2cf8be87-4b74-4c87-8780-9f2d9a08a01a",
   ollamaBaseUrl: process.env.OLLAMA_BASE_URL || "http://localhost:11434",
